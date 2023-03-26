496efc0effe8882212128d9f6eb0adbe5618c80c
shuling127 <65823900+shuling127@users.noreply.github.com>
2023-03-26 15:22:09 +0800

    Fix graph no edges and adjust data generation
    - Handle graph no edges error in route visualisation
    - Included gender and changed skills and expertises

--- RouteVisualisation.py
+++ RouteVisualisation.py
@@ -2,6 +2,7 @@ import json
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from itertools import cycle
 
 import geopandas as gpd
 from shapely.geometry import Point, LineString
@@ -9,6 +10,7 @@ import plotly_express as px
 import networkx as nx
 import osmnx as ox
 import folium
+import folium.plugins as plugins
 
 def to_time(time_window):
     """
@@ -29,8 +31,7 @@ def create_popup(text):
                         max_width=300)
     return popup
 
-
-def visualise_routes(json_result, polygon, addresses_list):
+def visualise_routes(json_result, polygon):
     """
     Visualise realistic routes for each phlebotomist and saves them to a .html file
 
@@ -39,6 +40,8 @@ def visualise_routes(json_result, polygon, addresses_list):
         polygon: geojson polygon provided by TATA
         addressess_list: list of addressess compiled - refer to Run Algorithm.ipynb 
     """
+    locations = json_result['Metadata']['Locations']
+
     # create base graph
     x_min, y_min, x_max, y_max = polygon.total_bounds
     G = ox.graph_from_bbox(north=y_max, south=y_min, east=x_max, west=x_min, network_type='drive')
@@ -48,7 +51,6 @@ def visualise_routes(json_result, polygon, addresses_list):
         'blue',
         'gray',
         'orange',
-        'beige',
         'darkblue',
         'lightblue',
         'purple',
@@ -56,13 +58,17 @@ def visualise_routes(json_result, polygon, addresses_list):
         'pink',
         'cadetblue',
         'lightgray',
-        'black'
+        'black',
+        'darkgreen'
     ]
+
+    colours_cycle = cycle(colours)
+
     alpha = 0.5
 
     # catchment area 
     catchment_popup_text = "Catchment Area"
-    catchment_coords = addresses_list[0].split(',')
+    catchment_coords = locations[0]['Coordinate'].split(',')
     catchment_lat, catchment_long = float(catchment_coords[0]), float(catchment_coords[1])
 
     for j in range(len(json_result['Routes'])):
@@ -77,19 +83,23 @@ def visualise_routes(json_result, polygon, addresses_list):
         start_times = phleb['Start Times Sequence']
         end_times = phleb['End Times Sequence']
 
-        colour = colours[j]
-        print(colour)
+        colour = next(colours_cycle)
 
+        sequence_counter = 0
         for i in range(len(locations_sequence)-1):
-            start = addresses_list[locations_sequence[i]].split(',')
-            end = addresses_list[locations_sequence[i+1]].split(',')
+
+            start = locations[locations_sequence[i]]['Coordinate'].split(',')
+            end = locations[locations_sequence[i+1]]['Coordinate'].split(',')
             start_lat, start_long = float(start[0]), float(start[1])
             end_lat, end_long = float(end[0]), float(end[1])
+            print(f"Coords: {start_lat},{start_long}")
 
             start_node = ox.distance.nearest_nodes(G, Y=start_lat, X=start_long)
             end_node = ox.distance.nearest_nodes(G, Y=end_lat, X=end_long)
+            print("Node: ", start_node, end_node)
 
             route = nx.shortest_path(G, start_node, end_node, weight='distance')
+            print("Route: ",route)
 
             arrival_time = f"Arrival:{to_time(start_times[i])}"
             departure_time = f"Departure:{to_time(end_times[i])}"
@@ -97,12 +107,17 @@ def visualise_routes(json_result, polygon, addresses_list):
             location_number = locations_sequence[i]
             order_id = json_result['Metadata']['Locations'][location_number]['Order Id']
 
+            if start_node == end_node:
+                print("Warning: Start and end node are the same")
+
+            # plotting route from phlebotomist home
             if i == 0:
-                # phlebotomist home
-                print(f"Map created for Phlebotomist ID #{phleb_id}'s route")
+                print(f"Drawing Phlebotomist ID #{phleb_id}'s route...")
                 if j == 0:
+                    print("Map created")
+                    # create map if map has not been created
                     route_map = ox.plot_route_folium(G, route, route_linewidth=6, node_size=0, color=colour, opacity=alpha)
-                else: 
+                elif start_node != end_node: 
                     route_map = ox.plot_route_folium(G, route, route_linewidth=6, node_size=0, route_map=route_map, color=colour, opacity=alpha)
                 # create markers
                 start_marker = folium.Marker(
@@ -113,15 +128,25 @@ def visualise_routes(json_result, polygon, addresses_list):
                 start_marker.add_to(route_map)
             else:
                 # if map has been created, add onto route map
-                route_map = ox.plot_route_folium(G, route, route_linewidth=6, node_size=0, route_map=route_map, color=colour, opacity=alpha)
+                if start_node != end_node:
+                    route_map = ox.plot_route_folium(G, route, route_linewidth=6, node_size=0, route_map=route_map, color=colour, opacity=alpha)
                 start_marker = folium.Marker(
                     location=(start_lat, start_long), # only accepts coords in tuple form
                     popup=create_popup(f'Order #{order_id}<br>{arrival_time}<br>{departure_time}'),
-                    icon = folium.Icon(color=colour, icon='user', prefix='fa')
+                    # icon = folium.Icon(color=colour, icon='user', prefix='fa'),
+                    icon = plugins.BeautifyIcon(
+                            icon="arrow-down", icon_shape="marker",
+                            number=sequence_counter,
+                            border_color=colour,
+                            background_color=colour,
+                            text_color='white'
+                    )
                 )
                 start_marker.add_to(route_map)
+                sequence_counter += 1
 
                 if i == last_order_index:
+                    print(f"Phlebotomist ID #{phleb_id}'s route has been drawn")
                     catchment_popup_text += f"<br>Phlebotomist #{phleb_id} Arrival:{to_time(start_times[i+1])}"
                     print(f"Last order for Phlebotomist ID #{phleb_id} fulfilled")
                     # save map at the end of last order
@@ -134,20 +159,47 @@ def visualise_routes(json_result, polygon, addresses_list):
                 )
     catchment_marker.add_to(route_map)
     route_map.save(f"Route Visualisations/Route.html")
-   
+
+####################################################################################################
+
+def create_grid_df(size, selected_dots):
+    grid = pd.DataFrame()
+    size += 1
+    grid['x'] = np.repeat(np.arange(0, size), size)
+    grid['y'] = np.tile(np.arange(0, size), size)
+    grid['Location'] = 0
+
+    for location_type, coords_list in selected_dots.items():
+        for coord in coords_list:
+            grid.loc[(grid['x'] == coord[0]) & (grid['y'] == coord[1]), 'Location'] = location_type
+            print(location_type)
+
+    print(grid)
+    return grid
+
+def create_addressess_list(selected_dots):
+    pass
+
+def visualise_boxdot():
+    pass
+
+
 # for testing purposes
 if __name__ == "__main__":
-    with open("Route Visualisations/matching.json", "r") as read_file:
+    # test visualise_routes()
+    with open("Route Visualisations/matching1.json", "r") as read_file:
         json_result = json.load(read_file)
     json_result = json.loads(json_result)
 
     polygon = gpd.read_file("Simulation\Gurugram_sample_Polygon.geojson")
-
-    addresses_list = ['28.484314452017287,77.08784537403447', '28.443423074966024,77.0864228696552', '28.44779634378927,77.04657462495449', '28.437283115842664,77.08363561384037', '28.426139873942272,77.09989980027176', '28.450975296139998,77.05707277162473', '28.4346276220027,77.10731046594978', '28.433803303996754,77.0815831101854', '28.440829814217103,77.05009232467279', '28.425023397747474,77.0933768886317', '28.437934021117723,77.08513002952219', '28.477749854110492,77.09063434989348', '28.48996623734903,77.0921778477132', '28.43207724256467,77.10740559477985', '28.44319553731333,77.07262034644641', '28.441460262857042,77.11217116399158', '28.418827407440464,77.08643447272122', '28.48019713242638,77.0907176889603', '28.451745009255756,77.06841123738455']
     
-    visualise_routes(json_result, polygon, addresses_list)
+    visualise_routes(json_result, polygon)
 
-def create_grid_df(size, ):
-    grid = pd.DataFrame()
-    grid['x'] = np.repeat(np.arange(0, size), size)
-    grid['y'] = np.tile(np.arange(0, size), size)
+    # # test visualise_boxdot
+    # scenario_1 = {
+    #     "Customer Orders":[(0,0),(0,1),(0,2),(0,3),(0,4),(0,5),(1,4),(3,3),(3,4),(4,3),(4,4),(5,3),(5,4)],
+    #     "Phlebotomist Home":[(1,1),(5,5)],
+    #     "Catchment Area":[(2,3)]
+    # }
+
+    # create_grid_df(5, scenario_1)
